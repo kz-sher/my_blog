b6773238db49b68d58adeec2827b15d4664f03e7
kz-sher <kzsherdev@gmail.com>
2021-05-23 23:57:06 +0800

    modify structure for md heading with link feature

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 prebuild:
 	python3 scripts/tag_generator.py
-	python3 scripts/linked_md_heading_substitutor.py
+	python3 scripts/md_heading_with_link_substitutor.py
 
 build: prebuild
 	bundle exec jekyll build
--- scripts/md_heading_with_link_substitutor.py
+++ scripts/md_heading_with_link_substitutor.py
@@ -1,21 +1,23 @@
 import re
 import glob 
+import os
 
 def sub_md_heading_with_link(content: str, level: int):
-    return re.sub('^' + '#' * level + ' (.*)', r'{% include linked_md_heading.html heading="\1" level=' + str(level) + r' %}', content, flags = re.M)
+    return re.sub('^' + '#' * level + ' (.*)', r'{% include md_heading_with_link.html heading="\1" level=' + str(level) + r' %}', content, flags = re.M)
 
-filenames = glob.glob('_posts/*md')
+filenames = glob.glob('_posts_bak/*md')
 for filename in filenames:
     # read post md content
     rf = open(filename, 'r', encoding='utf8')
-    # replace md heading with linked one 
 
+    # replace md heading with linked one 
     content = ''
     for line in rf:
         for i in range(1, 7): 
             line = sub_md_heading_with_link(line, i)
         content += line
+
     # write updated content back to post md
-    wf = open(filename, 'w', encoding='utf8')
+    wf = open('_posts/' + os.path.split(filename)[-1], 'w', encoding='utf8')
     wf.write(content)
     wf.close()
